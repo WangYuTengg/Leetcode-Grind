7307a9e37b488be124872514b451543b62a8864e
Wang Yu Teng <wyuteng123@gmail.com>
2023-02-08 20:53:58 +0800

    -

--- Array/IntersectionOfArrays2.py
+++ Array/IntersectionOfArrays2.py
@@ -1,6 +1,8 @@
 # Given two integer arrays nums1 and nums2, return an array of their intersection.
 # Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
 
+
+# O(MlogM + NlogN) time complexity 
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         nums1.sort()
@@ -17,3 +19,5 @@ class Solution:
                 i += 1
                 j += 1
         return ans
+    
+# O(M + N) time complexity
